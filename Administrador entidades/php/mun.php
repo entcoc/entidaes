b41dfec75b841e212f0b762a2165f4438172c91c
Jimmy Acero <jim.acero.valcarcel@gmail.com>
2017-05-27 12:00:41 -0500

    Añadir municipios terminado

--- Administrador entidades/php/mun.php
+++ Administrador entidades/php/mun.php
@@ -1,8 +1,20 @@
 <?php
 include('server.php');
 extract($_GET);
-$s=$db->prepare('insert into municipio  (nom_mun) values (?)');
-if($s->execute(array(htmlentities($mun)))){
+$query='insert into municipio  (dep_mun,nom_mun) values ';
+$muns=explode(",", $mun);
+$inserts=array();
+for($i=0;$i<count($muns);$i++){
+	if($i==0){
+		$query.='(?,?)';
+	}
+	else
+		$query.=',(?,?)';
+	array_push($inserts, $dep);
+	array_push($inserts, htmlentities($muns[$i]));
+}
+$s=$db->prepare($query);
+if($s->execute($inserts)){
 	echo 1;
 }
 else{
