922907786bb9097b44909192c7301d4b5a0ffc38
joav <jim.acero.valcarcel@gmail.com>
2017-06-02 02:44:34 -0500

    Ultimos arreglos administrador

--- Administrador entidades/js/script.js
+++ Administrador entidades/js/script.js
@@ -8,10 +8,12 @@ function decode_utf8(s) {
 	return decodeURIComponent(escape(s));
 }
 function simple(table,id,suf){
-	var q='select * from '+table+' where ? order by nom_'+suf;
-	console.log(q);
+	var q='select * from '+table+' where ?';
 	$.get("php/query.php",{q:q,v:'1'},function(d){
 		d=JSON.parse(d);
+		d.sort(function(a,b){
+			return (a['nom_'+suf]<=b['nom_'+suf])?-1:1;
+		});
 		var sel=$('#'+id);
 		for(var i=0;i<d.length;i++){
 			if(d[i]['id_'+suf]>0)
@@ -23,16 +25,18 @@ function simple(table,id,suf){
 	});
 }
 function municipio(){
-	var q='select * from municipio where ? order by nom_mun';
-	console.log(q);
+	var q='select * from municipio where ?';
 	$.get("php/query.php",{q:q,v:'1'},function(d){
 		d=JSON.parse(d);
+		d.sort(function(a,b){
+			return (a.nom_mun>=b.nom_mun);
+		});
 		var sel=$('#mun_ent');
 		for(var i=0;i<d.length;i++){
 			if(d[i].id_mun>0)
-				sel.append('<option data-dep='+d[i].dep_mun+' value='+d[i].id_mun+'>'+d[i].nom_mun+'</option>');
+				sel.append('<option style="display:none;" data-dep='+d[i].dep_mun+' value='+d[i].id_mun+'>'+d[i].nom_mun+'</option>');
 			else
-				sel.prepend('<option  data-dep='+d[i].dep_mun+' value='+d[i].id_mun+' selected>'+d[i].nom_mun+'</option>');
+				sel.prepend('<option style="display:none;" data-dep='+d[i].dep_mun+' value='+d[i].id_mun+' selected>'+d[i].nom_mun+'</option>');
 
 		}
 	});
