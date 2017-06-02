9c24e61b12617db6cb83e627c2a742410559e38e
joav <jim.acero.valcarcel@gmail.com>
2017-06-01 23:46:26 -0500

    Actualización insertar caracter

--- Administrador entidades/php/car.php
+++ Administrador entidades/php/car.php
@@ -1,7 +1,18 @@
 <?php
 include('server.php');
 extract($_GET);
-$s=$db->prepare('insert into caracter  (nom_car) values (?)');
+$query='insert into caracter  (nom_car) values ';
+$car=explode(",", $car);
+$inserts=array();
+for($i=0;$i<count($car);$i++){
+	if($i==0){
+		$query.='(?)';
+	}
+	else
+		$query.=',(?)';
+	array_push($inserts, htmlentities($car[$i]));
+}
+$s=$db->prepare($query);
 if($s->execute(array(htmlentities($car)))){
 	echo 1;
 }
